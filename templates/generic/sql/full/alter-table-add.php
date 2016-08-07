8c1a8f6ad067abe9a520d1841e630eb8f56fb9ef
David Neilsen <petah.p@gmail.com>
2016-08-07 13:18:53 +1200

    Fix alter table

--- templates/generic/sql/full/alter-table-add.php
+++ templates/generic/sql/full/alter-table-add.php
@@ -1,16 +1,15 @@
 <?php $previous = null; ?>
 <?php foreach ($entity->getAttributes() as $attribute): ?>
-<?php if (!$previous) { $previous = $attribute->getColumnName(); continue; } ?>
 ALTER TABLE <?= $entity->getTableName(); ?>
 
 <?php if ($attribute instanceof \Rhino\Codegen\Attribute\IntAttribute): ?>
-ADD <?= $attribute->getColumnName(); ?> INT NULL AFTER <?= $previous; ?>;
+ADD <?= $attribute->getColumnName(); ?> INT NULL AFTER <?= $previous ?: 'id'; ?>;
 <?php elseif ($attribute instanceof \Rhino\Codegen\Attribute\BoolAttribute): ?>
-ADD <?= $attribute->getColumnName(); ?> TINYINT(1) UNSIGNED NULL AFTER <?= $previous; ?>;
+ADD <?= $attribute->getColumnName(); ?> TINYINT(1) UNSIGNED NULL AFTER <?= $previous ?: 'id'; ?>;
 <?php elseif ($attribute instanceof \Rhino\Codegen\Attribute\TextAttribute): ?>
-ADD <?= $attribute->getColumnName(); ?> MEDIUMTEXT NULL AFTER <?= $previous; ?>;
+ADD <?= $attribute->getColumnName(); ?> MEDIUMTEXT NULL AFTER <?= $previous ?: 'id'; ?>;
 <?php else: ?>
-ADD <?= $attribute->getColumnName(); ?> VARCHAR(255) NULL AFTER <?= $previous; ?>;
+ADD <?= $attribute->getColumnName(); ?> VARCHAR(255) NULL AFTER <?= $previous ?: 'id'; ?>;
 <?php endif; ?>
 <?php $previous = $attribute->getColumnName(); ?>
 
--- templates/generic/sql/full/alter-table-change.php
+++ templates/generic/sql/full/alter-table-change.php
@@ -1,16 +1,15 @@
 <?php $previous = null; ?>
 <?php foreach ($entity->getAttributes() as $attribute): ?>
-<?php if (!$previous) { $previous = $attribute->getColumnName(); continue; } ?>
 ALTER TABLE <?= $entity->getTableName(); ?>
 
 <?php if ($attribute instanceof \Rhino\Codegen\Attribute\IntAttribute): ?>
-CHANGE <?= $attribute->getColumnName(); ?> <?= $attribute->getColumnName(); ?> INT NULL AFTER <?= $previous; ?>;
+CHANGE <?= $attribute->getColumnName(); ?> <?= $attribute->getColumnName(); ?> INT NULL AFTER <?= $previous ?: 'id'; ?>;
 <?php elseif ($attribute instanceof \Rhino\Codegen\Attribute\BoolAttribute): ?>
-CHANGE <?= $attribute->getColumnName(); ?> <?= $attribute->getColumnName(); ?> TINYINT(1) UNSIGNED NULL AFTER <?= $previous; ?>;
+CHANGE <?= $attribute->getColumnName(); ?> <?= $attribute->getColumnName(); ?> TINYINT(1) UNSIGNED NULL AFTER <?= $previous ?: 'id'; ?>;
 <?php elseif ($attribute instanceof \Rhino\Codegen\Attribute\TextAttribute): ?>
-CHANGE <?= $attribute->getColumnName(); ?> <?= $attribute->getColumnName(); ?> MEDIUMTEXT NULL AFTER <?= $previous; ?>;
+CHANGE <?= $attribute->getColumnName(); ?> <?= $attribute->getColumnName(); ?> MEDIUMTEXT NULL AFTER <?= $previous ?: 'id'; ?>;
 <?php else: ?>
-CHANGE <?= $attribute->getColumnName(); ?> <?= $attribute->getColumnName(); ?> VARCHAR(255) NULL AFTER <?= $previous; ?>;
+CHANGE <?= $attribute->getColumnName(); ?> <?= $attribute->getColumnName(); ?> VARCHAR(255) NULL AFTER <?= $previous ?: 'id'; ?>;
 <?php endif; ?>
 <?php $previous = $attribute->getColumnName(); ?>
 
