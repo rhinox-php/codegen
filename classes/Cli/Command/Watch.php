bc45bb431a466e16c1d9b4819c1c79392e21f498
David Neilsen <d.neilsen@we-are-mea.com>
2017-08-15 22:24:58 +1200

    Fix watch command

--- classes/Cli/Command/Watch.php
+++ classes/Cli/Command/Watch.php
@@ -5,19 +5,29 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\ArrayInput;
 
 class Watch extends AbstractCommand {
     protected function configure() {
         $this->setName('watch')
             ->setDescription('Watch code for changes and trigger generation automatically')
+            ->addOption('execute', 'x', InputOption::VALUE_NONE, 'Execute code generation (otherwise dry run).')
             ->addOption('schema', 's', InputOption::VALUE_REQUIRED, 'Codegen schema file to load.', 'codegen.php')
             ->addOption('debug', 'd', InputOption::VALUE_NONE, 'Enable debug output');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output) {
-        $codegen = getCodegen($input->getOption('schema'), true, $input->getOption('debug'));
-        $watcher = new \Rhino\Codegen\Watch\Watcher(function() {
-            passthru('php ' . __FILE__ . ' gen -x');
+        $codegen = $this->getCodegen($input->getOption('schema'), !$input->getOption('execute'), $input->getOption('debug'));
+        $watcher = new \Rhino\Codegen\Watch\Watcher(function() use($input, $output) {
+            $command = $this->getApplication()->find('gen');
+            $arguments = array(
+                'command' => 'gen',
+                '--execute' => $input->getOption('execute'),
+                '--schema' => $input->getOption('schema'),
+                '--debug' => $input->getOption('debug'),
+            );
+            $genInput = new ArrayInput($arguments);
+            $returnCode = $command->run($genInput, $output);
         });
         $watcher->addDirectory(__DIR__ . '/../');
         $watcher->addDirectory('.');
