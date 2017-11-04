56a3985031984d743363cba119294cb836e22cca
David Neilsen <d.neilsen@we-are-mea.com>
2017-11-04 23:51:20 +1300

    Dont lint templates

--- bin/lint-php-fix.sh
+++ bin/lint-php-fix.sh
@@ -2,6 +2,5 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 pushd $DIR/..
 vendor/bin/php-cs-fixer fix classes --rules=@PSR2,ordered_class_elements
-vendor/bin/php-cs-fixer fix templates --rules=@PSR2,ordered_class_elements
 vendor/bin/php-cs-fixer fix bin --rules=@PSR2,ordered_class_elements
 popd
