2c8579c7fd588c2655cb8679557bbad735cb44c8
David Neilsen <d.neilsen@we-are-mea.com>
2017-11-07 20:49:50 +1300

    Version check

--- bin/rhino-codegen.php
+++ bin/rhino-codegen.php
@@ -1,4 +1,8 @@
 <?php
+if (version_compare(PHP_VERSION, '7.1.0', '<')) {
+    echo 'Codegen require PHP 7.1+' . PHP_EOL;
+    return;
+}
 if (!class_exists('Rhino\Codegen\Codegen')) {
     if (is_file(__DIR__ . '/../vendor/autoload.php')) {
         require __DIR__ . '/../vendor/autoload.php';
