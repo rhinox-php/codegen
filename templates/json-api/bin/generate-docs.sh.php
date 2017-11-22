e578b9faffc67c77ed57e8d1561fd7949c0aa32d
David Neilsen <d.neilsen@we-are-mea.com>
2017-11-22 22:08:00 +1300

    Docs

--- templates/json-api/bin/generate-docs.sh.php
+++ templates/json-api/bin/generate-docs.sh.php
@@ -1,6 +1,7 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 pushd $DIR/..
+rm -rf docs/output
 mkdir -p docs/output
 node bin/generate-docs.js
 ./node_modules/.bin/bootprint openapi docs/output/api.yml docs/output
--- templates/json-api/docs/paths.yml.php
+++ templates/json-api/docs/paths.yml.php
@@ -13,12 +13,42 @@
       '200':
         description: List of <?= $entity->getLabel(); ?>
 
+        schema:
+          type: object
+          properties:
+            data:
+              type: array
+              items:
+                $ref: "#/definitions/<?= $entity->getClassName(); ?>"
+            included:
+              type: array
+              items:
+                anyOf:
+<?php foreach ($entity->getRelationships() as $relationship): ?>
+                  - $ref: '#/definitions/<?= $relationship->getClassName(); ?>Relationship'
+<?php endforeach; ?>
+
 /<?= $entity->getRouteName(); ?>/get/{id}:
   get:
     summary: Get a <?= $entity->getLabel(); ?> by ID
     responses:
       '200':
         description: Single of <?= $entity->getLabel(); ?>
+
+        schema:
+          type: object
+          properties:
+            data:
+              $ref: "#/definitions/<?= $entity->getClassName(); ?>"
+            included:
+              type: array
+              items:
+                anyOf:
+<?php foreach ($entity->getRelationships() as $relationship): ?>
+<?php if ($entity == $relationship->getFrom()): ?>
+                  - $ref: '#/definitions/<?= $relationship->getClassName(); ?>Relationship'
+<?php endif; ?>
+<?php endforeach; ?>
 
 /<?= $entity->getRouteName(); ?>/create:
   post:
@@ -28,6 +58,21 @@
       '200':
         description: New <?= $entity->getLabel(); ?>
 
+        schema:
+          type: object
+          properties:
+            data:
+              $ref: "#/definitions/<?= $entity->getClassName(); ?>"
+            included:
+              type: array
+              items:
+                anyOf:
+<?php foreach ($entity->getRelationships() as $relationship): ?>
+<?php if ($entity == $relationship->getFrom()): ?>
+                  - $ref: '#/definitions/<?= $relationship->getClassName(); ?>Relationship'
+<?php endif; ?>
+<?php endforeach; ?>
+
 /<?= $entity->getRouteName(); ?>/update/{id}:
   post:
     summary: Update a <?= $entity->getLabel(); ?>
@@ -36,6 +81,21 @@
       '200':
         description: Updated <?= $entity->getLabel(); ?>
 
+        schema:
+          type: object
+          properties:
+            data:
+              $ref: "#/definitions/<?= $entity->getClassName(); ?>"
+            included:
+              type: array
+              items:
+                anyOf:
+<?php foreach ($entity->getRelationships() as $relationship): ?>
+<?php if ($entity == $relationship->getFrom()): ?>
+                  - $ref: '#/definitions/<?= $relationship->getClassName(); ?>Relationship'
+<?php endif; ?>
+<?php endforeach; ?>
+
 /<?= $entity->getRouteName(); ?>/delete/{id}:
   post:
     summary: Delete a <?= $entity->getLabel(); ?>
@@ -43,5 +103,20 @@
     responses:
       '200':
         description: Deleted <?= $entity->getLabel(); ?>
+
+        schema:
+          type: object
+          properties:
+            data:
+              $ref: "#/definitions/<?= $entity->getClassName(); ?>"
+            included:
+              type: array
+              items:
+                anyOf:
+<?php foreach ($entity->getRelationships() as $relationship): ?>
+<?php if ($entity == $relationship->getFrom()): ?>
+                  - $ref: '#/definitions/<?= $relationship->getClassName(); ?>Relationship'
+<?php endif; ?>
+<?php endforeach; ?>
 
 <?php endforeach; ?>
