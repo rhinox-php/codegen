ac5ae829b35d31bacab725c07c49ebdaca6c3d04
David Neilsen <petah.p@gmail.com>
2018-09-16 21:06:45 +1200

    INdexes

--- templates/generic/sql/full/alter-table-index.php
+++ templates/generic/sql/full/alter-table-index.php
@@ -1,7 +1,5 @@
 <?php foreach ($entities as $entity): ?>
-<?php foreach ($entity->children('string', 'int', 'decimal', 'date', 'date-time', 'bool', 'text') as $attribute): ?>
-<?php if ($attribute->indexed): ?>
+<?php foreach ($entity->children('int') as $attribute): ?>
 ALTER TABLE `<?= $entity->table; ?>` ADD INDEX `<?= $attribute->column; ?>` (`<?= $attribute->column; ?>`);
-<?php endif; ?>
 <?php endforeach; ?>
 <?php endforeach; ?>
