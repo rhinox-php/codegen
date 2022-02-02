528915ce219790629e559896d409ce34debc2b94
David <d.c.neilsen@gmail.com>
2022-02-02 23:20:14 +1300

    Use php finder

--- classes/FormatPhp.php
+++ classes/FormatPhp.php
@@ -2,6 +2,8 @@
 
 namespace Rhino\Codegen;
 
+use Symfony\Component\Process\PhpExecutableFinder;
+
 class FormatPhp
 {
     public static function formatFile(string $file)
@@ -9,12 +11,13 @@ class FormatPhp
         if (!is_file($file)) {
             throw new \Exception('File not found to format: ' . $file);
         }
+        $php = (new PhpExecutableFinder())->find();
         if (strtolower(substr(PHP_OS, 0, 3)) === 'win') {
             assert(is_file(__DIR__ . '\..\vendor\bin\php-cs-fixer.bat'), new \Exception('Could not find php-cs-fixer, try running composer update.'));
             passthru(__DIR__ . '\..\vendor\bin\php-cs-fixer.bat fix ' . $file . ' --rules=@PSR2,ordered_class_elements > NUL 2> NUL');
         } else {
             assert(is_file(__DIR__ . '/../vendor/bin/php-cs-fixer'), new \Exception('Could not find php-cs-fixer, try running composer update.'));
-            passthru('php ' . __DIR__ . '/../vendor/bin/php-cs-fixer fix --quiet --config ' . __DIR__ . '/../bin/lint-php.config.php ' . $file . ' 2>&1');
+            passthru($php . ' ' . __DIR__ . '/../vendor/bin/php-cs-fixer fix --quiet --config ' . __DIR__ . '/../bin/lint-php.config.php ' . $file . ' 2>&1');
         }
     }
 
